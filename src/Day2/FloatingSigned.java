0ae49bc75d9c300ae2ffc64325ccae2060e79904
Vishu Goyal <goalsachiever8@gmail.com>
2021-12-28 17:40:51 +0530

    some changes in float code

--- src/Day2/FloatingSigned.java
+++ src/Day2/FloatingSigned.java
@@ -5,14 +5,16 @@ public class FloatingSigned {
     public static void main(String[] args)
     {
         //float
-        System.out.println("\n\n\nfloat operation ");
-
+        System.out.println("\nfloat operation ");
         float w1 = 0.123456789f;
-        float q = 12.123456789f;
-        float r = 1.123456789f;
+        float q = (float)12.123456789;
+        float r = 1.123456789F;
         System.out.println(w1);
         System.out.println(q );
         System.out.println(r);
+
+        //double
+        System.out.println("\ndouble operation ");
         double w2 = 2.123456789123456489 ;
         System.out.println(w2);
 
