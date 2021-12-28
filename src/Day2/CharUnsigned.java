8626995e8ad0da517f0603968d6434ea3b8eef56
Vishu Goyal <goalsachiever8@gmail.com>
2021-12-28 19:30:27 +0530

    some changes in char code

--- src/Day2/CharUnsigned.java
+++ src/Day2/CharUnsigned.java
@@ -13,7 +13,7 @@ public class CharUnsigned {
         char d = 32;                 //size--2bytes
         //  char e = 65537;            //compile time error(range 0 to 65535)
         char f = 65535;
-        System.out.println(d);      // space ASCII code  
+        System.out.println(d);      // space ASCII code
         System.out.println(f);
     }
 }
