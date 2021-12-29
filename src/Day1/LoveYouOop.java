ea24c5a30fe826a58af8f723f1c68db98e1c6e1a
Vishu Goyal <goalsachiever8@gmail.com>
2021-12-29 21:49:03 +0530

    LoveYouOop.java  code file is added

--- src/Day1/LoveYouOop.java
+++ src/Day1/LoveYouOop.java
@@ -2,7 +2,9 @@ package Day1;
 
 public class LoveYouOop {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         System.out.println("I Love You Java (OOP)");
     }
 }
