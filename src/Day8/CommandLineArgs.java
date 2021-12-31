f8ea6c0848c989fafc6aaea9052fa53c47f7aef1
Vishu Goyal <goalsachiever8@gmail.com>
2021-12-31 22:26:37 +0530

    CommandLineArgs.java  code file is added

--- src/Day8/CommandLineArgs.java
+++ src/Day8/CommandLineArgs.java
@@ -5,6 +5,18 @@ public class CommandLineArgs
     public static void main(String[] args)
     {
 
+        // Ways to convert String to Integer
+        //Integer i = Integer.parseInt(args[0]);
+        //Integer j = Integer.valueOf(args[1]);
+
+        int sum = 0;
+        for (String ele : args )
+        {
+            sum += Integer.parseInt(ele);
+        }
+
+        System.out.println("Sum of the all command line argument is : "+sum);
+
     }
 
 }
