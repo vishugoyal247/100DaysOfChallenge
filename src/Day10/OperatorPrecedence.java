85544613703158f71aa128c8f788b9c7b8ac81c0
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-03 13:10:18 +0530

    OperatorPrecedence.java  code file is added

--- src/Day10/OperatorPrecedence.java
+++ src/Day10/OperatorPrecedence.java
@@ -5,5 +5,58 @@ public class OperatorPrecedence
     public static void main(String[] args)
     {
 
+       /*
+       unary operator        - most
+       Binary operator
+       Ternary operator
+       Assignment operator   - least
+       */
+
+        /*
+        Precedence--
+
+        1. Unary Operator
+        [] ,x++,x--,++x,--x,~,!,new,<type>
+
+        2. Arithmetic Operator
+        *, /,%,+,-
+
+        3. Shift Operator
+        >>,>>>,<<
+
+        4. Comparison Operator
+        <,<=,>,>=,instanceof
+
+        5. Equality Operator
+        ==,!=
+
+        6. Bitwise Operator
+        &,^,|
+
+        7. Short Circuit Operator
+        &&,||
+
+        8. Conditional Operator
+        ? :
+
+        9. Assignment Operator
+        =,+=,-=,*=
+
+        */
+
+        int a=1,b=2,c=3,d=4,e=5,f=6;
+        System.out.println(++a*b);
+        System.out.println(c+=d);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(m1(1)+m1(2)*m1(3)/m1(4)+m1(5)*m1(6));
     }
+    public static int m1(int i)
+    {
+        System.out.println(i);
+        return i;
+    }
+    // In java,only we have operator precedence not an operand precedence
+    // Before apply any operator all operand evaluated(left to right)
+
 }
