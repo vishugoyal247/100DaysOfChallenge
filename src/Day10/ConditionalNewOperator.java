8569bd02eac0640cf5562384443c00336b8f6bde
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-03 13:10:55 +0530

    ConditionalNewOperator.java  code file is added

--- src/Day10/ConditionalNewOperator.java
+++ src/Day10/ConditionalNewOperator.java
@@ -1,4 +1,36 @@
 package Day10;
 
-public class ConditionalNewOperator {
+public class ConditionalNewOperator
+{
+    public static void main(String[] args)
+    {
+
+        /*
+        Conditional Operator (? :)
+        - int x=(condition)?true:false;
+        - nesting of operator is possible
+        */
+        int x = (5<6)?5:6;
+        int y = (1>2)?3:((4>5)?6:7);
+        System.out.println(x+" "+y);
+
+
+        /*
+        new operator
+        - use new operator to create object
+        - after creating object constructor will be executing to perform initialization of object
+        - we have new but not delete , destructor of keyword is managed by garbage collector
+        */
+        ConditionalNewOperator can =new ConditionalNewOperator();
+        System.out.println(can);
+
+
+        /*
+        [] operator
+        - used to create and declare this array
+        */
+        int[] arr = new int[10];
+        System.out.println(arr);
+
+    }
 }
